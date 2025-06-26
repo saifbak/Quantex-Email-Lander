1207eec02fc0d7eb26f15e03c8c8b44b5235a701
saifbak <saif@softbuilders.ae>
2025-06-27 00:02:06 +0400

    new videos added

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Index1 from "./pages/Landing1/Index";
 import Index2 from "./pages/Landing2/Index";
 import Index3 from "./pages/Landing3/Index";
 import Index4 from "./pages/Landing4/Index";
+import Index5 from "./pages/Landing5/Index";
+import Index6 from "./pages/Landing6/Index";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -22,6 +24,8 @@ const App = () => (
           <Route path="/mai-hendy" element={<Index2 />} />
           <Route path="/rehab-nasr-eldin" element={<Index3 />} />
           <Route path="/shainnah-magleo" element={<Index4 />} />
+          <Route path="/nouran-fahmy" element={<Index5 />} />
+          <Route path="/sherine-emad" element={<Index6 />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
---
+++ src/pages/Landing6/Index.tsx
@@ -0,0 +1,17 @@
+
+import Hero from "@/components/Hero6";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen relative">
+      {/* Split background */}
+      <div className="absolute inset-0 bg-gradient-to-b from-[#1A335A] from-50% to-white to-50%"></div>
+      <div className="relative z-10">
+        <div className="hidden md:flex absolute w-full h-2.5 top-1/2 mt-7 -translate-y-1/2 bg-gradient-to-r from-purple-500 via-blue-400 to-yellow-400  items-center justify-center"></div>
+        <Hero />
+      </div>
+    </div>
+  );
+};
+
+export default Index;
