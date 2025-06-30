0bd6928b7f27ea56a0af6ea12d586243039ff5c8
saifbak <saif@softbuilders.ae>
2025-06-30 15:56:39 +0400

    new video and email template added

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ import Index3 from "./pages/Landing3/Index";
 import Index4 from "./pages/Landing4/Index";
 import Index5 from "./pages/Landing5/Index";
 import Index6 from "./pages/Landing6/Index";
+import Index7 from "./pages/Landing7/Index";
+import Index8 from "./pages/Landing8/Index";
+import Index9 from "./pages/Landing9/Index";
+import Index10 from "./pages/Landing10/Index";
 import NotFound from "./pages/NotFound";
 import EmailTemplatePage from "./pages/Email1";
 import EmailTemplatePage2 from "./pages/Email2";
@@ -16,6 +20,10 @@ import EmailTemplatePage3 from "./pages/Email3";
 import EmailTemplatePage4 from "./pages/Email4";
 import EmailTemplatePage5 from "./pages/Email5";
 import EmailTemplatePage6 from "./pages/Email6";
+import EmailTemplatePage7 from "./pages/Email7";
+import EmailTemplatePage8 from "./pages/Email8";
+import EmailTemplatePage9 from "./pages/Email9";
+import EmailTemplatePage10 from "./pages/Email10";
 
 const queryClient = new QueryClient();
 
@@ -32,13 +40,20 @@ const App = () => (
           <Route path="/shainnah-magleo" element={<Index4 />} />
           <Route path="/nouran-fahmy" element={<Index5 />} />
           <Route path="/sherine-emad" element={<Index6 />} />
+          <Route path="/abdulla-jihad-zainab" element={<Index7 />} />
+          <Route path="/shainnah-mae-magleo" element={<Index8 />} />
+          <Route path="/rehab-nasreldeen-rateb-abdelaleem" element={<Index9 />} />
+          <Route path="/mai-mohamed-fathi-mohamed-elsayed" element={<Index10 />} />
           <Route path="/email-abdullah-zainab" element={<EmailTemplatePage />} />
           <Route path="/email-mai-hendy" element={<EmailTemplatePage2 />} />
           <Route path="/email-rehab-nasr-eldin" element={<EmailTemplatePage3 />} />
           <Route path="/email-shainnah-magleo" element={<EmailTemplatePage4 />} />
           <Route path="/email-nouran-fahmy" element={<EmailTemplatePage5 />} />
           <Route path="/email-sherine-emad" element={<EmailTemplatePage6 />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/email-abdulla-jihad-zainab" element={<EmailTemplatePage7 />} />
+          <Route path="/email-shainnah-mae-magleo" element={<EmailTemplatePage8 />} />
+          <Route path="/email-rehab-nasreldeen-rateb-abdelaleem" element={<EmailTemplatePage9 />} />
+          <Route path="/email-mai-mohamed-fathi-mohamed-elsayed" element={<EmailTemplatePage10 />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
